cd2b20b7141af6449bb23330f28502bd10691e1e
GreedyAi <m.vato122@gmail.com>
2022-01-08 17:14:33 +0400

    initial commit

---
+++ apps/margvel/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'margvel-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'margvel';
+}
